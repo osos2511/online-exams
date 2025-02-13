3e22a3a9254c78ad9efaa92551ef27f9c4236d97
Osama mohamed <om329211@gmail.com>
2025-02-13 12:52:09 +0200

    +signUp

---
+++ lib/auth/widgets/Custom_TextForm_Field.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField({super.key,
+    required this.validator,
+    required this.controller,
+    this.isSecure = false,
+    required this.hintText,
+    required this.labelText});
+  final String hintText;
+  final String labelText;
+  final String? Function(String?)? validator;
+  final TextEditingController? controller;
+  final bool isSecure;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  late bool _obSecureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obSecureText = widget.isSecure;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: widget.validator,
+      controller: widget.controller,
+      obscureText: _obSecureText,
+      decoration: InputDecoration(
+        label: Text(widget.labelText),
+        labelStyle: TextStyle(fontWeight: FontWeight.w500),
+        hintText: widget.hintText,
+        hintStyle: TextStyle(
+          color: Color(0xff535353),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(width: 2,color: Color(0xff535353)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(width:2,color: Color(0xff02369C)),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(width: 2, color: Colors.red),
+        ),
+        suffixIcon: widget.isSecure
+            ? IconButton(
+          icon: Icon(
+            _obSecureText ? Icons.visibility_off : Icons.visibility,color: Colors.white,
+          ),
+          onPressed: () {
+            setState(() {
+              _obSecureText = !_obSecureText;
+            });
+          },
+        )
+            : null,
+      ),
+    );
+  }
+}
---
+++ lib/core/Custom_AppBar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final String titleAppbar;
+  final IconData? iconAppbar;
+  final void Function()? onPressAppbar;
+
+  const CustomAppbar({super.key, required this.titleAppbar,this.iconAppbar,this.onPressAppbar});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(40),
+      child: AppBar(
+        leadingWidth: 0,
+        leading: IconButton(onPressed: onPressAppbar, icon: Icon(iconAppbar)),
+        title: Text(
+          titleAppbar,style: TextStyle(color: Color(0xFF02369C),fontWeight: FontWeight.w500,fontSize: 30),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
